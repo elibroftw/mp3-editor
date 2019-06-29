e59653912435beb2016a0e97bcee7fddff5a6b83
elibroftw <xxelibroxx@gmail.com>
2019-06-29 10:34:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def individual_select(filename):
                 on_menu = False
             else:
                 print(individual_select_menu_text)
-            if 0 < sub_menu_user_choice < 15:
+            if 0 < sub_menu_user_choice < 16:
                 print('Enter an option')
             else: print('Please enter an integer from 1 to 15')
         except ValueError: print('Please enter an integer from 1 to 15')
